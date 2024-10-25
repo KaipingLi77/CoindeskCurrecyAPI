f95334a05bdfd34c2237428c51fcab3f91f81887
KaiPing Lee <s4951758@gmail.com>
2024-10-25 18:02:07 +0800

    輸出CoindeskController UnitTest後的資料

--- src/test/java/org/example/currency_price_api/CoindeskControllerTest.java
+++ src/test/java/org/example/currency_price_api/CoindeskControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -38,21 +39,23 @@ public class CoindeskControllerTest {
     }
 
     @Test
-    public void testGetCurrencyFromCoindesk() throws Exception {
+    public void testGetCurrencyFromCoindesk() throws Exception  {
         String mockResponse = "{ \"time\": { \"updated\": \"Oct 25, 2024 09:02:33 UTC\" }, \"bpi\": { \"USD\": { \"code\": \"USD\", \"rate\": \"67,586.575\" } } }";
 
         // 模擬 CoindeskService 返回的值
         when(coindeskService.getCurrentPriceFromCoindesk()).thenReturn(mockResponse);
 
         // 使用 MockMvc 發送 GET 請求到 /api/coindesk
-        mockMvc.perform(get("/api/coindesk")
+        MvcResult response = mockMvc.perform(get("/api/coindesk")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()) // 驗證狀態碼是否為 200
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // 驗證返回的 Content-Type
                 .andExpect(jsonPath("$.bpi.USD.code").value("USD")) // 驗證返回的 JSON 中的 bpi.USD.code 是否為 "USD"
-                .andExpect(jsonPath("$.bpi.USD.rate").value("67,586.575")); // 驗證返回的 JSON 中的 bpi.USD.rate 是否為 "67,586.575"
+                .andExpect(jsonPath("$.bpi.USD.rate").value("67,586.575")) // 驗證返回的 JSON 中的 bpi.USD.rate 是否為 "67,586.575"
+                .andReturn();
+        String responseContent = response.getResponse().getContentAsString();
+        System.out.println(responseContent);
     }
-
     @Test
     public void testGetTransformedCurrencyFromCoindesk() throws Exception {
         String mockResponse = "[{\"code\":\"USD\",\"chineseName\":\"美金\",\"rate\":\"67,586.575\"}," +
@@ -61,7 +64,7 @@ public class CoindeskControllerTest {
 
         when(coindeskService.getTransformedDataWithCoindesk()).thenReturn(mockResponse);
 
-        mockMvc.perform(get("/api/coindesk/transformed"))
+        MvcResult response = mockMvc.perform(get("/api/coindesk/transformed"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].code").value("USD")) // 驗證 JSON 返回中的第一個對象的 code 是否是 "USD"
@@ -70,6 +73,9 @@ public class CoindeskControllerTest {
                 .andExpect(jsonPath("$[1].code").value("GBP")) // 驗證第二個對象的 code 是否是 "GBP"
                 .andExpect(jsonPath("$[1].chineseName").value("英鎊")) // 驗證第二個對象的 chineseName 是否是 "英鎊"
                 .andExpect(jsonPath("$[2].code").value("EUR")) // 驗證第三個對象的 code 是否是 "EUR"
-                .andExpect(jsonPath("$[2].chineseName").value("歐元")); // 驗證第三個對象的 chineseName 是否是 "歐元"
+                .andExpect(jsonPath("$[2].chineseName").value("歐元"))// 驗證第三個對象的 chineseName 是否是 "歐元"
+                .andReturn();
+        String responseContent = response.getResponse().getContentAsString();
+        System.out.println(responseContent);
     }
 }
